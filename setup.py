a8315ea01babfea34ce2d35b9cd70049b812b273
Friedrich Lindenberg <friedrich@pudo.org>
2016-12-29 11:48:58 +0100

    add russian company types

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fingerprints',
-    version='0.3.2',
+    version='0.3.3',
     description="A library to generate entity fingerprints.",
     long_description="",
     classifiers=[
