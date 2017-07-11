b5591e2f77afde52de23f94034a4fbe7bfa3f7fb
Friedrich Lindenberg <friedrich@pudo.org>
2017-07-11 13:07:21 +0200

    up the version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fingerprints',
-    version='0.5.1',
+    version='0.5.2',
     description="A library to generate entity fingerprints.",
     long_description="",
     classifiers=[
