8cd34849e46cf30f72afda157c78ab40c6ee083c
Friedrich Lindenberg <friedrich@pudo.org>
2017-09-01 09:21:35 +0200

    make a release

--- setup.py
+++ setup.py
@@ -3,23 +3,24 @@ from setuptools import setup, find_packages
 
 setup(
     name='fingerprints',
-    version='0.5.2',
+    version='0.5.3',
     description="A library to generate entity fingerprints.",
     long_description="",
     classifiers=[
-        "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4'
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     keywords='names people companies normalisation',
     author='Friedrich Lindenberg',
     author_email='friedrich@pudo.org',
-    url='http://github.com/pudo/fingerprints',
+    url='http://github.com/alephdata/fingerprints',
     license='MIT',
     packages=find_packages(exclude=['ez_setup', 'examples', 'test']),
     namespace_packages=[],
