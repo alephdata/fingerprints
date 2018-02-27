d120a2540546546ada5a85552b6ccd9171eef95f
Friedrich Lindenberg <friedrich@pudo.org>
2018-02-27 13:25:08 +0100

    cut a release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fingerprints',
-    version='0.5.3',
+    version='0.5.4',
     description="A library to generate entity fingerprints.",
     long_description="",
     classifiers=[
