de9a74f0d9fd8984ab512711080487521329933b
Friedrich Lindenberg <friedrich@pudo.org>
2018-09-11 08:40:53 +0200

    travis ping pong

--- fingerprints/cleanup.py
+++ fingerprints/cleanup.py
@@ -13,10 +13,10 @@ PREFIXES_RAW = ['Mr', 'Mrs', 'Mister', 'Miss', 'Madam', 'Madame',
                 'Monsieur', 'Mme', 'Mmme', 'Herr', 'Hr', 'Frau',
                 'Fr', 'The', u'Fräulein', 'Senor', 'Senorita',
                 'Sr', 'Sir', 'Lady', 'The', 'A', 'de', 'of']
-PREFIXES_RAW = '|'.join(PREFIXES_RAW)
+PREFIXES_RAW = '|'.join(PREFIXES_RAW).encode('utf-8')
 NAME_PATTERN = r'^\W*((%s)\.?\s+)*(?P<term>.*?)([\'’]s)?\W*$'
 NAME_PATTERN = NAME_PATTERN % PREFIXES_RAW
-PREFIXES = re.compile(NAME_PATTERN.encode('utf-8'), re.I | re.U)
+PREFIXES = re.compile(NAME_PATTERN, re.I | re.U)
 
 
 def clean_entity_name(name):
