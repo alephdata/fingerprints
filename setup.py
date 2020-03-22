8352332814ca7224b332e1e53b7aeda58a714e8d
Friedrich Lindenberg <friedrich@pudo.org>
2020-03-22 14:25:13 +0100

    Bump version: 0.6.6 → 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='fingerprints',
-    version='0.6.6',
+    version='1.0.0',
     description="A library to generate entity fingerprints.",
     long_description=long_description,
     long_description_content_type='text/markdown',
