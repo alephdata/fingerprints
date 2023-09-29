48ad1fb58d68986c3b3f16072ac5cfc56ffc4746
Friedrich Lindenberg <friedrich@pudo.org>
2023-09-29 12:31:40 +0200

    test cleanup methods a tiny bit

---
+++ tests/test_cleanup.py
@@ -0,0 +1,11 @@
+from fingerprints import clean_name_ascii, clean_name_light
+
+
+def test_clean_name_ascii():
+    assert clean_name_ascii("Владимир Путин") == "vladimir putin"
+    assert clean_name_ascii("Владимир Пути'н") == "vladimir putin"
+
+
+def test_clean_name_light():
+    assert clean_name_light("Vladimir Putin") == "vladimir putin"
+    assert clean_name_light("C.I.A.") == "cia"
